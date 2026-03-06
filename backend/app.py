6656902e4d517a8dce1c9937115f0504a6654a3e
otonashi8 <otonashi8898@gmail.com>
2026-03-06 00:57:52 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -135,5 +135,5 @@ def quiz():
     return jsonify(word)
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
