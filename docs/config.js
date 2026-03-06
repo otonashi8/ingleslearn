5a6ef3b644d4c94b6ec31037083828588ff6f4a9
otonashi8 <otonashi8898@gmail.com>
2026-03-06 01:42:20 -0500

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,12 +1,3 @@
-// CONFIGURACIÓN - CAMBIA ESTO CUANDO TENGAS TU BACKEND EN RENDER
 const CONFIG = {
     API_URL: 'https://ingleslearn-api.onrender.com'
 };
-
-// No cambiar esto
-if (window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1') {
-    // Si no es localhost, asumimos que es producción
-    CONFIG.API_URL = `https://${window.location.hostname.replace('frontend', 'api')}`;
-
-}
-
